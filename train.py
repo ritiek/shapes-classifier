1343e630bd3af9c939380e367ec6a0d73adddfd8
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2018-09-17 23:10:19 +0530

    Better model

--- train.py
+++ train.py
@@ -67,10 +67,13 @@ from keras import layers
 from keras import regularizers
 
 model = models.Sequential()
-model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(10,10,256)))
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(32, (2, 2), activation='relu', input_shape=(10, 10, 256)))
+model.add(layers.MaxPooling2D((1, 1)))
+model.add(layers.Conv2D(64, (2, 2), activation='relu'))
+model.add(layers.MaxPooling2D((1, 1)))
+model.add(layers.Conv2D(64, (2, 2), activation='relu'))
 model.add(layers.Flatten())
-model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(4, activation='softmax'))
 
 model.compile(optimizer='rmsprop',
